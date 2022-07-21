14a1e38107300bbac52fe083fcf32b5c1f49ee6c
alvinmo <alvinmo@tencent.com>
2022-07-21 14:26:40 +0800

    Initial Commit

---
+++ Shared/MetaMaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaMaskApp.swift
+//  Shared
+//
+//  Created by 莫熙涛 on 2022/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MetaMaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
